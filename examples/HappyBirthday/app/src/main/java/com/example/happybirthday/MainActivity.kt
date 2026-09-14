355350d433a8b445be4330f812420ddd8ea4ff06
James Anderson <james.anderson@wright.edu>
2026-09-14 12:56:22 -0400

    some comments on what we did in class for the start of the Birthday Card demo

--- examples/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ examples/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +21,50 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HappyBirthdayTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding -> // innerPadding might be underlined in red, but we can ignore that for now
+
                 }
             }
         }
     }
 }
 
+/**
+ * Composable for the app text
+ */
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GreetingText(
+    from: String,
+    message: String,
+    modifier: Modifier = Modifier,
+) {
+    Column()
+    {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp
+        )
+
+        Text(
+            text = from,
+            fontSize = 36.sp
+        )
+    }
 }
 
+/**
+ * Preview function
+ *
+ * We can call a Composable function, and it will show up in the preview pane
+ */
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        Greeting("Android")
+        GreetingText(
+            message = "Happy Birthday James!",
+            from = "From Paris"
+        )
     }
 }
